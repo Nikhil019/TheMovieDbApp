ccc49ec448f8f8569539ed179c9bf28b5946aae8
Nikhil019 <40515387+Nikhil019@users.noreply.github.com>
2018-08-08 19:09:15 +0530

    Update ListLoadListener.java

--- app/src/main/java/com/example/nikhil/themoviedb/ListLoadListener.java
+++ app/src/main/java/com/example/nikhil/themoviedb/ListLoadListener.java
@@ -5,9 +5,6 @@ import com.example.nikhil.themoviedb.entities.TvShows.Shows;
 
 import java.util.List;
 
-/**
- * Created by shubham on 7/23/2018.
- */
 
 public interface ListLoadListener {
     public void onMoviesListLoaded(List<Movie> movies);
